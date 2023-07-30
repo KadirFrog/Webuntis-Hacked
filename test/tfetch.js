1dac39d3ff99af7fc85aac94b6b6c8614588d120
KadirFrog <yanarkadiraras@gmail.com>
2023-07-30 21:10:42 +0300

    add test and fix some bugs

---
+++ test/tfetch.js
@@ -0,0 +1,14 @@
+import { WebUntisAnonymousAuth, WebUntisElementType } from "webuntis";
+
+async function getTable() {
+    try {
+      const untis = new WebUntisAnonymousAuth('Koenigin-Katharina-Stift', 'kadmos.webuntis.com');
+      await untis.login();
+      const classes = await untis.getClasses();
+      const timetable = await untis.getTimetableForToday(classes[0].id, WebUntisElementType.CLASS);
+      console.log(timetable);
+    } catch (error) {
+      alert('Error occurred:', error);
+    }
+  }
+  
