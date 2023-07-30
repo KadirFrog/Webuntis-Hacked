e0997e802c233d0437c0169fc8f257f9315a14d0
KadirFrog <yanarkadiraras@gmail.com>
2023-07-30 18:47:47 +0300

    fetch edit

--- data/fetch.js
+++ data/fetch.js
@@ -1,7 +1,8 @@
 import { WebUntisAnonymousAuth, WebUntisElementType } from 'webuntis';
 
-const untis = new WebUntisAnonymousAuth('school', 'xyz.webuntis.com');
-
+function getTable() 
+const untis = new WebUntisAnonymousAuth('school', 'kadmos.webuntis.com');{
 await untis.login();
 const classes = await untis.getClasses();
 const timetable = await untis.getTimetableForToday(classes[0].id, WebUntisElementType.CLASS);
+}
