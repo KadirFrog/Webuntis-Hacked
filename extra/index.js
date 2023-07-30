03ec4fd8b8e90cbbdde93e4ddd49d421b6beae0f
KadirFrog <yanarkadiraras@gmail.com>
2023-07-30 20:32:55 +0300

    dclass

---
+++ extra/index.js
@@ -0,0 +1,11 @@
+import * as fetchjs from "../data/fetch.js"
+
+function getValue() {
+    const selectElement = document.getElementById("Klasse");
+    const selectedValue = selectElement.value;
+    return selectedValue;
+}
+
+function load() {
+    
+}
