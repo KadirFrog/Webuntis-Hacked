c1c7c9a777fa7f294fb8aebf8f43e2292cf64a62
KadirFrog <yanarkadiraras@gmail.com>
2023-07-30 20:58:12 +0300

    school name

--- data/fetch.js
+++ data/fetch.js
@@ -3,7 +3,7 @@ import * as indexjs from '../index.js';
 
 async function getTable() {
     try {
-      const untis = new WebUntisAnonymousAuth('school', 'kadmos.webuntis.com');
+      const untis = new WebUntisAnonymousAuth('Koenigin-Katharina-Stift', 'kadmos.webuntis.com');
       await untis.login();
       const classes = await untis.getClasses();
       const timetable = await untis.getTimetableForToday(classes[0].id, WebUntisElementType.CLASS);
