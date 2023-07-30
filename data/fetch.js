7ad53dada065db2bfc2800337b992fddeeccad83
KadirFrog <yanarkadiraras@gmail.com>
2023-07-30 19:42:17 +0300

    fetch fix

--- data/fetch.js
+++ data/fetch.js
@@ -1,8 +1,17 @@
 import { WebUntisAnonymousAuth, WebUntisElementType } from 'webuntis';
+import * as indexjs from '../index.js';
+// Assuming you have imported the required WebUntisAnonymousAuth class and other dependencies
 
-function getTable() 
-const untis = new WebUntisAnonymousAuth('school', 'kadmos.webuntis.com');{
-await untis.login();
-const classes = await untis.getClasses();
-const timetable = await untis.getTimetableForToday(classes[0].id, WebUntisElementType.CLASS);
-}
+async function getTable() {
+    try {
+      const untis = new WebUntisAnonymousAuth('school', 'kadmos.webuntis.com');
+      await untis.login();
+      const classes = await untis.getClasses();
+      const timetable = await untis.getTimetableForToday(classes[0].id, WebUntisElementType.CLASS);
+      console.log(timetable); // Do something with the timetable data
+    } catch (error) {
+      console.error('Error occurred:', error);
+      // Handle the error or display a user-friendly message
+    }
+  }
+  
